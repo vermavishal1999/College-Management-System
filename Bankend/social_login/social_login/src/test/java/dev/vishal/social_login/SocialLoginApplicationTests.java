e5d9265b559459361aee70771e33033c26bc4d3b
Vishal Verma <vermavishal11021999@gmail.com>
2025-01-18 12:31:25 +0530

    comment 1

---
+++ Bankend/social_login/social_login/src/test/java/dev/vishal/social_login/SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vishal.social_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
